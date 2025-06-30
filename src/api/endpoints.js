ef45520ae481692bc038e27f089d0e9a5d84b7a1
Chathura99 <chathura.manohara@omobio.net>
2025-06-30 23:53:00 +0530

    images

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,7 +1,7 @@
 // src/api/endpoints.js
 
-export const USERS_ENDPOINT = 'users';
-export const TABLE_ENDPOINT = 'items/tables';
-export const ORDER_ENDPOINT = 'items/orders';
-export const MENU_CATEGORY_ENDPOINT = 'items/categories';
-export const FILE_UPLOAD_ENDPOINT = 'files';
+export const USERS_ENDPOINT = '/users';
+export const TABLE_ENDPOINT = '/items/tables';
+export const ORDER_ENDPOINT = '/items/orders';
+export const MENU_CATEGORY_ENDPOINT = '/items/categories';
+export const FILE_UPLOAD_ENDPOINT = '/files';
